6e5de39a831e1a9b755ed367eefdb1e27c6389b9
ktoroshchin <k.toroshchin@gmail.com>
2021-01-11 13:27:18 -0500

    MongoDB created in GCP Cluster

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,11 +1,26 @@
 import express from 'express'
+import 'express-async-errors'
 import { json } from 'body-parser'
+import { currentUserRouter } from './routes/current-user'
+import { signinRouter } from './routes/signin'
+import { signoutRouter } from './routes/signout'
+import { signupRoute } from './routes/signup'
+import { errorHandler } from './middleware/error-handler'
+import { NotFoundError } from './errors/not-found-error'
 
 const app = express()
 app.use(json())
 
-app.get('/api/users/currentuser', (req, res) => {
-    res.send('Hello world')
+app.use(currentUserRouter)
+app.use(signinRouter)
+app.use(signoutRouter)
+app.use(signupRoute)
+
+app.all('*', async(req, res) => {
+    throw new NotFoundError()
 })
+
+app.use(errorHandler)
+
 
 app.listen(3000, () => console.log('Listening on port 3000'))
