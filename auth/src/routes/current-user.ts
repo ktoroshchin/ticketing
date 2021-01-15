88cbb8208b562d97bde7af4fb28b43db165d2e56
ktoroshchin <k.toroshchin@gmail.com>
2021-01-15 16:12:49 -0500

    current user route

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,22 @@
 import express from 'express'
+import jwt from 'jsonwebtoken'
 
 const router = express.Router()
 
 router.get('/api/users/currentuser', (req, res) => {
-    res.send('hi current user route')
+    if(!req.session?.jwt) {
+        res.send({ currentUser: null })
+    }
+
+    try {
+        // will throw error if token is invalid
+        const payload = jwt.verify(req.session?.jwt, process.env.jwt_key!)
+        res.send({ currentUser: payload})
+
+    } catch (err) {
+        res.send({ currentUser: null })
+    }
+
 })
 
 export { router as currentUserRouter }
--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,8 +1,10 @@
 import express, { NextFunction, Request, Response } from 'express'
-import { body, validationResult } from 'express-validator'
-import { RequestValidationError } from '../errors/request-validation-error'
+import { body } from 'express-validator'
 import { validateRequest } from '../middleware/validation-request'
 import { User } from '../models/user'
+import { BadRequestError } from '../errors/bad-request-error'
+import { Password } from '../services/password'
+import jwt from 'jsonwebtoken'
 
 const router = express.Router()
 
@@ -16,13 +18,34 @@ router.post('/api/users/signin',[
         .withMessage('Password is required'),
         validateRequest
 ], 
-(req: Request, res: Response, next: NextFunction) => {
+async (req: Request, res: Response, next: NextFunction) => {
     //validateRequest function could also go here
+    const { email, password } = req.body
+    const existingUser = await User.findOne({ email })
+    
+    if(!existingUser) {
+        throw new BadRequestError('Invalid credentials')
+    }
+
+    const passwordMatch = await Password.compare(existingUser.password, password)
+    if(!passwordMatch) {
+        throw new BadRequestError('Invalid credentials')
+    }
+
+    // Create json web token
+    const userJwt = jwt.sign({
+        id: existingUser.id,
+        email: existingUser.email
+    },
+        process.env.jwt_key!
+    )
+    // store it on session object
+    req.session = {
+        jwt: userJwt
+    }
 
 
-
-
-    res.send({})
+    res.status(200).send(existingUser)
 })
 
 export { router as signinRouter }
