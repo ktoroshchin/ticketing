0d8717bcb2cca9862a1c414c75b6a99a2b0ebb57
ktoroshchin <k.toroshchin@gmail.com>
2021-01-19 10:28:07 -0500

    singout logic

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,22 +1,10 @@
 import express from 'express'
-import jwt from 'jsonwebtoken'
+import { currentUser } from '../middleware/current-user'
 
 const router = express.Router()
 
-router.get('/api/users/currentuser', (req, res) => {
-    if(!req.session?.jwt) {
-        res.send({ currentUser: null })
-    }
-
-    try {
-        // will throw error if token is invalid
-        const payload = jwt.verify(req.session?.jwt, process.env.jwt_key!)
-        res.send({ currentUser: payload})
-
-    } catch (err) {
-        res.send({ currentUser: null })
-    }
-
+router.get('/api/users/currentuser', currentUser, (req, res) => {
+   res.status(201).send({ currentUser: req.currentUser || null })
 })
 
 export { router as currentUserRouter }
